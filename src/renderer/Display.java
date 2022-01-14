3e634603379e003620bf0430c8e2a7f1ed4bd773
kaabeche <rayanoo1302@gmail.com>
2022-01-14 20:27:52 +0100

    test

---
+++ src/renderer/Display.java
@@ -0,0 +1,106 @@
+package renderer;
+
+import org.w3c.dom.html.HTMLDOMImplementation;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+
+public class Display extends Canvas implements Runnable {
+    private static final long serialVersionUID=1L;
+
+
+    private Thread thread;
+    private JFrame frame;
+    private static String title= "3d renderer";
+    private static final int WIDTH=800;
+    private static final int HEIGHT=600;
+    private static  boolean running = false;
+
+    public Display(){
+        this.frame = new JFrame();
+
+        Dimension size = new Dimension(WIDTH,HEIGHT);
+        this.setPreferredSize(size);
+    }
+
+
+    public static void main(String[] args) {
+        Display display = new Display();
+        display.frame.setTitle(title);
+        display.frame.add(display);
+        display.frame.pack();
+        display.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        display.frame.setLocationRelativeTo(null);
+        display.frame.setResizable(false);
+        display.frame.setVisible(true);
+
+        display.start();
+
+    }
+
+    public synchronized void start(){
+        running=true;
+        this.thread = new Thread(this,"renderer.Display");
+        this.thread.start();
+    }
+
+
+
+    public synchronized void stop() throws InterruptedException {
+        running=false;
+        this.thread.join();
+    }
+
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime();
+        long timer = System.currentTimeMillis();
+        final double ns = 1000000000.0 / 60;
+        int frames = 0;
+        double delta=0;
+
+        while (running){
+            long now = System.nanoTime();
+            delta += (now - lastTime)/ns;
+            lastTime = now;
+            while (delta>=1){
+                update();
+                delta--;
+            }
+            render();
+            frames++;
+            if(System.currentTimeMillis()-timer > 1000){
+                timer+=1000;
+                this.frame.setTitle(title + " | fps: "+frames);
+                frames=0;
+            }
+        }
+        try {
+            stop();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs==null){
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,WIDTH, HEIGHT);
+
+        g.setColor(Color.BLUE);
+        g.fillRect(50,100,200, 200);
+
+        g.dispose();
+        bs.show();
+    }
+    private void update(){
+
+    }
+}
