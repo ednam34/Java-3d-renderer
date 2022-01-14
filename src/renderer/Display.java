460ba9680bf54068453da00582b806626809c2dd
KAABECHE rayane <rayanoo1302@gmail.com>
2022-01-14 20:38:40 +0100

    test2

--- src/renderer/Display.java
+++ src/renderer/Display.java
@@ -8,20 +8,20 @@ import java.awt.image.BufferStrategy;
 
 
 public class Display extends Canvas implements Runnable {
-    private static final long serialVersionUID=1L;
+    private static final long serialVersionUID = 1L;
 
 
     private Thread thread;
     private JFrame frame;
-    private static String title= "3d renderer";
-    private static final int WIDTH=800;
-    private static final int HEIGHT=600;
-    private static  boolean running = false;
+    private static String title = "3d renderer";
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    private static boolean running = false;
 
-    public Display(){
+    public Display() {
         this.frame = new JFrame();
 
-        Dimension size = new Dimension(WIDTH,HEIGHT);
+        Dimension size = new Dimension(WIDTH, HEIGHT);
         this.setPreferredSize(size);
     }
 
@@ -40,16 +40,15 @@ public class Display extends Canvas implements Runnable {
 
     }
 
-    public synchronized void start(){
-        running=true;
-        this.thread = new Thread(this,"renderer.Display");
+    public synchronized void start() {
+        running = true;
+        this.thread = new Thread(this, "renderer.Display");
         this.thread.start();
     }
 
 
-
     public synchronized void stop() throws InterruptedException {
-        running=false;
+        running = false;
         this.thread.join();
     }
 
@@ -59,22 +58,22 @@ public class Display extends Canvas implements Runnable {
         long timer = System.currentTimeMillis();
         final double ns = 1000000000.0 / 60;
         int frames = 0;
-        double delta=0;
+        double delta = 0;
 
-        while (running){
+        while (running) {
             long now = System.nanoTime();
-            delta += (now - lastTime)/ns;
+            delta += (now - lastTime) / ns;
             lastTime = now;
-            while (delta>=1){
+            while (delta >= 1) {
                 update();
                 delta--;
             }
             render();
             frames++;
-            if(System.currentTimeMillis()-timer > 1000){
-                timer+=1000;
-                this.frame.setTitle(title + " | fps: "+frames);
-                frames=0;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                this.frame.setTitle(title + " | fps: " + frames);
+                frames = 0;
             }
         }
         try {
@@ -84,23 +83,24 @@ public class Display extends Canvas implements Runnable {
         }
     }
 
-    private void render(){
+    private void render() {
         BufferStrategy bs = this.getBufferStrategy();
-        if(bs==null){
+        if (bs == null) {
             this.createBufferStrategy(3);
             return;
         }
         Graphics g = bs.getDrawGraphics();
         g.setColor(Color.BLACK);
-        g.fillRect(0,0,WIDTH, HEIGHT);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
 
         g.setColor(Color.BLUE);
-        g.fillRect(50,100,200, 200);
+        g.fillRect(50, 100, 200, 200);
 
         g.dispose();
         bs.show();
     }
-    private void update(){
+
+    private void update() {
 
     }
 }
